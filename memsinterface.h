1b6a282bc61f0b0fb3e4cb7c62206f24143b085b
mini56 <48644212+mini56@users.noreply.github.com>
2026-08-11 09:07:01 +0200

    Update memsinterface.h

--- memsinterface.h
+++ memsinterface.h
@@ -78,6 +78,7 @@ public slots:
 	void on_m_IACPlusButton_clicked();
 	void on_m_AllActuatorsOffButton_clicked();
 	void on_interactive_push_button_clicked();
+    void onProtocolCommandRequested(quint8 command); 
 signals:
     void dataReady();
     void connected();
