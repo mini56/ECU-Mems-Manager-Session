1f6ecf3e0c5db7e64f663afa377b3390787e4daf
mini56 <48644212+mini56@users.noreply.github.com>
2026-08-13 16:03:58 +0200

    Add numeric translation key API

--- i18n.h
+++ i18n.h
@@ -15,6 +15,7 @@ public:
     static bool load(const QString &languageCode);
     static QString text(const char *source);
     static QString text(const QString &source);
+    static QString text(int key);
     static QString language();
     static void install(QApplication *app);
     static void apply(QWidget *root);
@@ -27,10 +28,12 @@ private:
     static I18n *instance();
     bool loadDictionary(const QString &fileName);
     QString lookup(const QString &source) const;
+    QString lookupKey(int key) const;
     void applyObject(QObject *object);
 
     QString m_language;
     QHash<QString, QString> m_dictionary;
+    QHash<int, QString> m_keyDictionary;
     bool m_applying = false;
 };
 
