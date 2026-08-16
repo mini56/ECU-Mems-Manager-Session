43ff0f91b2f2f718ef4d54766797a552bfae0dc7
mini56 <48644212+mini56@users.noreply.github.com>
2026-08-16 09:42:28 +0200

    Add explicit final navigation installer header

---
+++ navigationorderpatch.h
@@ -0,0 +1,9 @@
+#ifndef NAVIGATIONORDERPATCH_H
+#define NAVIGATIONORDERPATCH_H
+
+class QApplication;
+class QMainWindow;
+
+void installFinalNavigation(QApplication *app, QMainWindow *window);
+
+#endif
