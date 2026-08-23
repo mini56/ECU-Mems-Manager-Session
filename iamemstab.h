e6ba5b6de62fec2dddfc4f4dacf6df5648afaca1
mini56 <48644212+mini56@users.noreply.github.com>
2026-08-23 20:22:53 +0200

    Add IA MEMS conversational tab interface

---
+++ iamemstab.h
@@ -0,0 +1,90 @@
+#ifndef IAMEMSTAB_H
+#define IAMEMSTAB_H
+
+#include <QHash>
+#include <QList>
+#include <QPointer>
+#include <QString>
+#include <QWidget>
+
+#include "expert/ExpertEngine.h"
+#include "expert/ExpertKnowledgeReader.h"
+#include "expert/ExpertTypes.h"
+
+class QLabel;
+class QLineEdit;
+class QPushButton;
+class QTextBrowser;
+class MainWindow;
+class MEMSInterface;
+
+class IaMemsTab final : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit IaMemsTab(MainWindow *mainWindow, QWidget *parent = nullptr);
+    ~IaMemsTab() override;
+
+private slots:
+    void sendQuestion();
+    void captureEcuSample();
+    void onInjectionSample(double finalMs,
+                           double baseMs,
+                           double transientMs,
+                           quint16 baseRaw,
+                           quint16 transientRaw,
+                           quint8 transientCounter);
+    void onEcuConnected();
+    void onEcuDisconnected();
+
+private:
+    void startKnowledgeLoad();
+    void finishKnowledgeLoad(bool ok, const QString &databasePath, const QString &errorText);
+    void appendMessage(const QString &speaker, const QString &text);
+    void appendSystemMessage(const QString &text);
+    void updateStatus();
+    void addObservation(const ExpertObservation &observation);
+    void updateContextFromQuestion(const QString &question);
+
+    QString answerQuestion(const QString &question);
+    QString currentValuesAnswer() const;
+    QString historyAnswer() const;
+    QString analysisAnswer(bool explainPrevious = false);
+    QString knowledgeAnswer(const QString &question) const;
+    QString helpAnswer() const;
+
+    QStringList knowledgeTerms(const QString &question) const;
+    QString verificationLabel(const QString &level) const;
+
+private:
+    MainWindow *m_mainWindow = nullptr;
+    MEMSInterface *m_mems = nullptr;
+
+    QTextBrowser *m_transcript = nullptr;
+    QLineEdit *m_question = nullptr;
+    QPushButton *m_sendButton = nullptr;
+    QLabel *m_status = nullptr;
+
+    ExpertEngine m_engine;
+    ExpertKnowledgeReader m_reader;
+    ExpertContext m_context;
+    QList<ExpertObservation> m_history;
+    ExpertAnalysisResult m_lastAnalysis;
+
+    QString m_databasePath;
+    QString m_knowledgeError;
+    bool m_knowledgeLoading = false;
+    bool m_knowledgeReady = false;
+    bool m_connected = false;
+
+    bool m_haveInjection = false;
+    double m_injectionFinalMs = 0.0;
+    double m_injectionBaseMs = 0.0;
+    double m_injectionTransientMs = 0.0;
+    quint8 m_injectionTransientCounter = 0;
+
+    static const int kMaximumSamples = 600;
+};
+
+#endif // IAMEMSTAB_H
