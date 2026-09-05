1b106eed05e1fd665b857f73a719f02ee6b6b2ac
mini56 <48644212+mini56@users.noreply.github.com>
2026-09-05 11:01:33 +0200

    Integrate provenance-filtered MEMSLibrary bridge for BUILD105

--- expert/IaMemsLibraryBridge.h
+++ expert/IaMemsLibraryBridge.h
@@ -6,7 +6,12 @@
 struct IaMemsLibraryGrounding
 {
     bool libraryReady = false;
+    bool provenanceFiltered = false;
     int resultCount = 0;
+    int selectedPage = -1;
+    QString selectedDocument;
+    QString selectedRevision;
+    QString selectedSourceLanguage;
     QString text;
     QString error;
 };
